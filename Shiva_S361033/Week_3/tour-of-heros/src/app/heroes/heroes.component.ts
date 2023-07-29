e54856333e80e3e5eb5de506c4293e43de00ce8d
Shiva Gupta <gshiva53@gmail.com>
2023-07-29 19:25:04 +0930

    [ADD]: tour-of-heros tutorial progress

--- Shiva_S361033/Week_3/tour-of-heros/src/app/heroes/heroes.component.ts
+++ Shiva_S361033/Week_3/tour-of-heros/src/app/heroes/heroes.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero'; 
+import { HeroService } from '../hero.service'; 
+import { MessageService } from '../message.service'; 
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1, 
-    name: 'Windstorm'
-  }; 
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+
+  heroes: Hero[] = []; 
+  // selectedHero?: Hero; 
+  
+  ngOnInit(): void {
+    this.getHeroes(); 
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);  
+  }
+  
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HerosComponent: Selected hero id= ${hero.id}`);
+  // }
 }
