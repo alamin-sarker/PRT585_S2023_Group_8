0910700cc2ecc51d85a202bee57b19d793fabdb6
Shiva Gupta <gshiva53@gmail.com>
2023-07-31 19:56:45 +0930

    [MODIFY]: Api tutorial complete

--- Shiva_S361033/Week_3/TodoApi/Program.cs
+++ Shiva_S361033/Week_3/TodoApi/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.EntityFrameworkCore; 
+using TodoApi.Models; 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList")); 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
