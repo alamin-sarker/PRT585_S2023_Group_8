eed8cc54a0677871b91265a82713a4dcbe4ff116
Shiva Gupta <gshiva53@gmail.com>
2023-09-03 19:27:38 +0930

    [ADD]: teacher model in 1CommonInfra

---
+++ Group_8_Software_Project/1CommonInfrastructure/Models/TeacherModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1CommonInfrastructure.Models
+{
+    public class TeacherModel
+    {
+        public int TeacherId { get; set; }
+        public string TeacherName { get; set; }
+
+    }
+
+}
