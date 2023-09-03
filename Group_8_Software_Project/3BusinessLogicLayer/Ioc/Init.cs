fd8e7ef627091eeb88f63d4923e38b14aeb2d82e
alamin-sarker <INferno@38>
2023-09-03 21:43:27 +0930

    T3.1 - Completed 2 screens each in the 3-tier web app

--- Group_8_Software_Project/3BusinessLogicLayer/Ioc/Init.cs
+++ Group_8_Software_Project/3BusinessLogicLayer/Ioc/Init.cs
@@ -14,7 +14,8 @@ namespace _3BusinessLogicLayer.Ioc
             services.AddScoped<IPersonService, PersonService>();
             services.AddScoped<IStudentService, StudentService>();
             services.AddScoped<IEmployeeService, EmployeeService>();
-
+            services.AddScoped<IUnitService, UnitService>();
+            services.AddScoped<ICourseService, CourseService>();
             services.AddScoped<ISecurityService, SecurityService>();
             //services.AddScoped<ICategoryService, CategoryService>();
 
