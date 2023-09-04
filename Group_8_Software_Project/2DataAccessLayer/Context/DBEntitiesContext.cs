ee2dd02ea5663272c53de20826d69432e1959409
Anup-kadariya <53438855+Anup-kadariya@users.noreply.github.com>
2023-09-04 18:57:43 +0930

    Making branch for 2 APIs

--- Group_8_Software_Project/2DataAccessLayer/Context/DBEntitiesContext.cs
+++ Group_8_Software_Project/2DataAccessLayer/Context/DBEntitiesContext.cs
@@ -20,6 +20,8 @@ namespace _2DataAccessLayer.Context
 
         public DbSet<Student> Students { get; set; }
 
+        public DbSet<Unit> Units { get; set; }
+        
         public DbSet<Employee> Employees { get; set; }
 
 
