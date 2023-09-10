d0aa3ef9f76b9d65919ec4caf907835d493469bb
subashkhatri <subash@khatri.com>
2023-09-10 11:21:01 +0930

    Add a book Model in Communication Layer - Add 3 fields with ID, Book Name and Author Name

---
+++ 1CommonInfrastructure/Models/SkBookModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1CommonInfrastructure.Models
+{
+    public class SkBookModel
+    {
+        public int BookId { get; set; } // int
+        public string BookName { get; set; } // nvarchar(400)
+         public string AuthorName { get; set; }
+
+    }
+
+}
