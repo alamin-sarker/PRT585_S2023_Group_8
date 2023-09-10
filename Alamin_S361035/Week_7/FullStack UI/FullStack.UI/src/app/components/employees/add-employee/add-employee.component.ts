3358f60cf8f8a1d27ad3350bf467a49c45cadc16
alamin-sarker <INferno@38>
2023-09-10 14:43:56 +0930

    fullstack .net and angular CURD operations worked

---
+++ Alamin_S361035/Week_7/FullStack UI/FullStack.UI/src/app/components/employees/add-employee/add-employee.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EmployeesService } from 'src/app/Services/employees.service';
+import { Employee } from 'src/app/models/employee.model';
+
+@Component({
+  selector: 'app-add-employee',
+  templateUrl: './add-employee.component.html',
+  styleUrls: ['./add-employee.component.css']
+})
+export class AddEmployeeComponent implements OnInit{
+
+  addEmployeeRequest: Employee = {
+    id: '',
+    name: '',
+    email: '',
+    phone: 0,
+    salary: 0,
+    department: ''
+  };
+
+
+  constructor(private employeeService: EmployeesService, 
+    private router: Router){}
+
+  ngOnInit(): void {
+    
+  }
+
+  addEmployee(){
+    this.employeeService.addEmployee(this.addEmployeeRequest)
+    .subscribe({
+      next: (employee) => {
+        this.router.navigate(['employees']); 
+        //console.log(employee);
+      }
+    });
+  }
+
+}
---
+++ Alamin_S361035/Week_7/FullStack UI/FullStackAPI/FullStack.API/FullStack.API/Migrations/20230910050135_Second Migration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FullStack.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Salary",
+                table: "Employees",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Phone",
+                table: "Employees",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Salary",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
