d538412b2849e6443df67f0cca4bda5967c9fefb
Anup-kadariya <53438855+Anup-kadariya@users.noreply.github.com>
2023-09-16 19:31:09 +0930

    adding ng zorro to project for dashbord

--- Group_8_Software_Project/AngularClientApp/src/app/app.module.ts
+++ Group_8_Software_Project/AngularClientApp/src/app/app.module.ts
@@ -5,6 +5,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UnitComponent } from './unit/unit.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -14,9 +25,11 @@ import { UnitComponent } from './unit/unit.component';
   imports: [
     BrowserModule,
     AppRoutingModule, 
-    HttpClientModule
+    HttpClientModule, FormsModule, BrowserAnimationsModule, IconsProviderModule, NzLayoutModule, NzMenuModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
