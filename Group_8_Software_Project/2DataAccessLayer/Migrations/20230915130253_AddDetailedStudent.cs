8e9dce162e410f37648fda42e42b4a1fb86d3e16
Shiva Gupta <gshiva53@gmail.com>
2023-09-17 20:11:54 +0930

    [MODIFY]: add migration for additional fields for student API

---
+++ Group_8_Software_Project/2DataAccessLayer/Migrations/20230915130253_AddDetailedStudent.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _2DataAccessLayer.Migrations
+{
+    public partial class AddDetailedStudent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StudentCourse",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StudentEmail",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StudentPhoneNumber",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StudentSemester",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StudentCourse",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "StudentEmail",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "StudentPhoneNumber",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "StudentSemester",
+                table: "Students");
+        }
+    }
+}
